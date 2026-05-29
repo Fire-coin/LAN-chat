78851b7837a0554fa466569a5a0bae3ff959241f
firecoin <fire.coin98@gmail.com>
2026-05-29 20:47:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# LAN-chat
+A simple P2P CLI tool that enables chatting and file sending across LAN network
+
+# Resources
+## Sockets on linux
+https://www.linuxhowtos.org/C_C++/socket.htm
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+int main() {
+
+  std::cout << "Start of the Test\n";
+
+  return 0;
+}
